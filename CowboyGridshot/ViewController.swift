d90461cd6b2178db85dda933c87fa3a512a3ee71
Justin Mercado <justinreianmercado@gmail.com>
2022-08-19 18:29:13 -0700

    Initial Commit

---
+++ CowboyGridshot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CowboyGridshot
+//
+//  Created by Justin Mercado on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
